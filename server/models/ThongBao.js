86b05dca2ca0562c416e4e32cd4a7538e9b0f5d2
NguyenSon0403 <son2004ntt@gmail.com>
2025-06-10 14:02:44 +0700

    fix conflic

--- server/models/ThongBao.js
+++ server/models/ThongBao.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-// ❌ Sai: bạn đang viết thế này
-// const router = require('router'); ← Sai thư viện
 
-// ✅ Đúng:
-=======
->>>>>>> 820bca714fa77e3868220336b80d8f14c2ee5027
 const mongoose = require("mongoose");
 
 const ThongBaoSchema = new mongoose.Schema({
--- server/server.js
+++ server/server.js
@@ -19,11 +19,6 @@ connectDB();
 
 // Middleware cho JSON
 app.use(express.json());
-<<<<<<< HEAD
-
-// Serve static files từ thư mục 'client'
-=======
->>>>>>> 820bca714fa77e3868220336b80d8f14c2ee5027
 app.use(express.static(path.join(__dirname, "../client")));
 
 // API Routes
